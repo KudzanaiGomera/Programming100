9b3caddc11fb734716c90f68835ab8232527d891
GOMERA <kgomera@corp.transunion.com>
2024-12-08 19:54:29 +0200

    build a calculator program

---
+++ datastructures..py
@@ -0,0 +1,14 @@
+# List - have brackets [] - everything inside is called an item
+
+movies = ["Avengers", "Deadpool", "The hangover", "Xmen"]
+print(movies[1]) #using index for the items, starts at 0
+print(movies[1:3]) #returns the first number given until right before the last number given
+
+# Tuples - Immutable (cannnot be change - ())
+coordinates = (40.7128, 74.0060) #new york
+red = (255, 0, 0)
+
+# dictionaries - key/value pairs {}
+
+drinks = {"White Russian": 8, "Old Fashion": 12, "Lemon Drop": 5} #drink is the key and the value is the price.
+print(drinks)
---
+++ importing.py
@@ -0,0 +1,6 @@
+# importing is important
+import sys #systems functions and parameters
+from datetime import datetime as dt #using an alise
+
+print(sys.version)
+print(dt.now())
